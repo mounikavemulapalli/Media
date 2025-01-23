f0125c41e8f893c2ae7c768639613387de322723
mounikavemulapalli <124512238+mounikavemulapalli@users.noreply.github.com>
2025-01-23 21:44:48 +0530

    changes committed

--- src/data/MenuItems.jsx
+++ src/data/MenuItems.jsx
@@ -9,51 +9,51 @@ import {
 
 export const menuItems = [
   {
-    title: "Ten Academy",
+    title: "Innovative Distribution Platforms",
     icon: <GraduationCap className='w-5 h-5' />,
     description:
-      "Transform your learning journey with Ten Academy innovative courses and expert-led training programs.",
-    url: "https://academy.entrepreneurshipnetwork.net",
+      "Leveraging state-of-the-art technology, our platforms distribute content seamlessly across various devices and channels. Our digital infrastructure ensures that your media is accessible and engaging, whether on a smartphone, tablet, or desktop.",
   },
-  // {
-  //   title: "Ten Mentor",
-  //   icon: <Users className='w-5 h-5' />,
-  //   description:
-  //     "Connect with industry experts and experienced mentors who guide you through your professional development.",
-  //   url: "https://mentor.entrepreneurshipnetwork.net",
-  // },
-  // {
-  //   title: "Ten Book Engine",
-  //   icon: <BookOpen className='w-5 h-5' />,
-  //   description:
-  //     "Access our vast digital library of educational resources, textbooks, and interactive learning materials.",
-  //   url: "https://ten-book-engine.vercel.app/",
-  // },
-  // {
-  //   title: "Ten Virtual Campus",
-  //   icon: <Building2 className='w-5 h-5' />,
-  //   description:
-  //     "Experience immersive learning in our state-of-the-art virtual campus environment.",
-  //   url: "https://discord.com/invite/wdgH95JY6a",
-  // },
-  // {
-  //   title: "Ten Virtual University",
-  //   icon: <School className='w-5 h-5' />,
-  //   description:
-  //     "Earn accredited degrees and certifications through our comprehensive online university programs.",
-  //   url: "https://ten-virtual-you-tube-university.vercel.app/",
-  // },
-  // {
-  //   title: "Ten Resume Builder",
-  //   icon: <FileText className='w-5 h-5' />,
-  //   description:
-  //     "Create professional resumes effortlessly with Ten Resume Builder.",
-  //   url: "https://resume.entrepreneurshipnetwork.net/",
-  // },
   {
-    title: "Ten Media",
+    title: "Audience Engagement Strategies",
+    icon: <Users className='w-5 h-5' />,
+    description:
+      "We develop and implement strategies to foster a deep connection between our content and our viewers. Our approach encourages active participation, feedback, and community building, ensuring a loyal and engaged audience.",
+    // url: "https://mentor.entrepreneurshipnetwork.net",
+  },
+  {
+    title: "Comprehensive Media Solutions:",
+    icon: <BookOpen className='w-5 h-5' />,
+    description:
+      "From concept to execution, we offer end-to-end solutions for all your media needs. Our services include content strategy, production, editing, and distribution, providing a one-stop-shop for all your digital media requirements.",
+    // url: "https://ten-book-engine.vercel.app/",
+  },
+  {
+    title: "Creative Consulting",
+    icon: <Building2 className='w-5 h-5' />,
+    description:
+      "Our team of experts offers consulting services to help you navigate the complex digital landscape. Whether you need advice on content creation, platform selection, or audience engagement, we're here to guide you every step of the way.",
+    // url: "https://discord.com/invite/wdgH95JY6a",
+  },
+  {
+    title: "Ten Virtual University",
+    icon: <School className='w-5 h-5' />,
+    description:
+      "Earn accredited degrees and certifications through our comprehensive online university programs.",
+    url: "https://ten-virtual-you-tube-university.vercel.app/",
+  },
+  {
+    title: "Ten Resume Builder",
     icon: <FileText className='w-5 h-5' />,
-    description: "Create Media platforms.",
+    description:
+      "Create professional resumes effortlessly with Ten Resume Builder.",
+    url: "https://resume.entrepreneurshipnetwork.net/",
+  },
+  {
+    title: "Digital Content Creation",
+    icon: <FileText className='w-5 h-5' />,
+    description:
+      "We craft high-quality, original content tailored to your audience's interests and preferences. From videos and articles to podcasts and interactive experiences, we ensure every piece resonates and captivates.",
     url: "",
   },
 ];
